1601a9b6905f1175475894d5b9314669f1a76f72
Jothi-Ram <ramukuttytheiva@gmail.com>
2020-05-01 14:57:12 +0530

    Final Commit

---
+++ calljs.js
@@ -0,0 +1,160 @@
+function first(x)
+{
+$(x).modal('show');
+}
+function second(x)
+{
+    $(x).modal('show');
+}
+function third(x)
+{
+    $(x).modal('show');
+}
+function forth(x)
+{
+    $(x).modal('show');
+}
+function five(x)
+{
+    $(x).modal('show');
+}
+function six(x)
+{
+    $(x).modal('show');
+}
+function seven(x)
+{
+    $(x).modal('show');
+}
+function eight(x)
+{
+    $(x).modal('show');
+}
+function nine(x)
+{
+    $(x).modal('show');
+}
+function ten(x)
+{
+    $(x).modal('show');
+}
+function eleven(x)
+{
+    $(x).modal('show');
+}
+function twelve(x)
+{
+    $(x).modal('show');
+}
+function thirteen(x)
+{
+    $(x).modal('show');
+}
+function fourteen(x)
+{
+    $(x).modal('show');
+}
+function fifteen(x)
+{
+    $(x).modal('show');
+}
+function sixteen(x)
+{
+    $(x).modal('show');
+}
+function seventeen(x)
+{
+    $(x).modal('show');
+}
+function eighteen(x)
+{
+    $(x).modal('show');
+}
+function nineteen(x)
+{
+    $(x).modal('show');
+}
+function twenty(x)
+{
+    $(x).modal('show');
+}
+function twentyone(x)
+{
+    $(x).modal('show');
+}
+function twentytwo(x)
+{
+    $(x).modal('show');
+}
+function twentythree(x)
+{
+    $(x).modal('show');
+}
+function twentyfour(x)
+{
+    $(x).modal('show');
+}
+function twentyfive(x)
+{
+    $(x).modal('show');
+}
+function twentysix(x)
+{
+    $(x).modal('show');
+}
+function twentyseven(x)
+{
+    $(x).modal('show');
+}
+function twentyeight(x)
+{
+    $(x).modal('show');
+}
+function twentynine(x)
+{
+    $(x).modal('show');
+}
+function thirty(x)
+{
+    $(x).modal('show');
+}
+function thirtyone(x)
+{
+    $(x).modal('show');
+}
+function thirtytwo(x)
+{
+    $(x).modal('show');
+}
+function thirtythree(x)
+{
+    $(x).modal('show');
+}
+function thirtyfour(x)
+{
+    $(x).modal('show');
+}
+function thirtyfive(x)
+{
+    $(x).modal('show');
+}
+function thirtysix(x)
+{
+    $(x).modal('show');
+}
+function thirtyseven(x)
+{
+    $(x).modal('show');
+}
+function thirtyeight(x)
+{
+    $(x).modal('show');
+}
+function thirtynine(x)
+{
+    $(x).modal('show');
+}
+function fourty(x)
+{
+    $(x).modal('show');
+}
---
+++ modelopen.js
@@ -0,0 +1,4 @@
+function open(x)
+{
+      $(x).model('');
+}
